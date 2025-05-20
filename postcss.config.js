67c1f411a31963253d50173ae008e58d4d56eddc
Unchained Learning <bboarer@outlook.com>
2025-05-20 11:38:13 +0100

    Purged css correctly

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,22 @@ const purgecss = require('@fullhuman/postcss-purgecss').default;
 module.exports = {
   plugins: [
     purgecss({
-      content: ['./**/*.html'],
+      content: ['./*.html', './*.js'],
+      safelist: [
+        'visible',
+        'hidden',
+        'fade',
+        'scroll-reveal',
+        'reveal',
+        'dark',
+        'light',
+        'data-theme',
+        'toolkit-reveal',
+        'forge-card',
+        'toolkit-toggle-wrapper',
+        'section-divider-svg',
+        'section-divider-wrapper'
+      ]
     })
   ]
 };
